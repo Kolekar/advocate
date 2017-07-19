140ee0b76501e9cd707d98237e12722e62948ded
kolekar <harikeshkolekar@gmail.com>
2017-07-19 15:01:59 +0000

    routes changed

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Myapp::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-
+root to: 'home#index'
 devise_for :users
 
 namespace :lawyer do
